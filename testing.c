77178ae6f4263164fb811ac01a33be21d07350ef
Ansah-98 <yungtupac99@gmail.com>
2023-08-21 10:42:07 -0700

    updates

--- testing.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-int main() {
-	_printf("Hello, %s!\n", "World");
-	_printf("The character is %c\n", 'A');
-	_printf("The value is %%d\n", 42);
-	_printf("Let's try to printf a simple sentence.\n");
-	return 0;
-}
